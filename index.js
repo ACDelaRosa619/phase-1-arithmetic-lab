7d9bef7e5195deebd7b19b4159e4b56113455b64
Adrian <acdelarosa19@gmail.com>
2023-09-19 01:13:20 -0700

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+let multiply = 62
+function multiplyTwoNumbers(num1, num2) {
+    return num1 * num2
+}
+console.log(multiplyTwoNumbers(31, 2))
+
+let random = 62
+function addingNumbersTogether(num1, num2) {
+    return num1 + num2
+}
+console.log(addingNumbersTogether(31, 2))
+
+const num3 = 48
+const num4 = 12
+
+let mod = 4
+function dividingNumbers(num3, num4) {
+    return num3 / num4
+}
+console.log(dividingNumbers(48,12))
+
+let max = 20
+counter = 0
+counter++ > 20
